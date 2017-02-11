94cf623f80451ec730c8a447b48703a822e8a46c
Chris Gradwohl <christophergradwohl@gmail.com>
2017-02-10 22:59:28 -0800

    read input with python

---
+++ types.py
@@ -0,0 +1,16 @@
+# Declare second integer, double, and String variables.
+j = 5
+e = 5.0
+t = 'python'
+
+# Read and save an integer, double, and String to your variables.
+inInt = raw_input()
+inDub = raw_input()
+inStr = raw_input()
+
+# Print the sum of both integer variables on a new line.
+print inInt, inDub, inStr 
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
